5e4b7d412ae2899f2c785e3e80d1cbac3f8fd61b
Milan Carapic <makzvakkvak@hotmail.com>
2022-05-12 16:56:10 +0200

    Domaci

---
+++ domaci_4/domaci_zbirOpsega/js/script.js
@@ -0,0 +1,12 @@
+let zbir=0
+function zbirOpsega(parametar_1,parametar_2){
+    for (let i=parametar_1;i<=parametar_2; i=i+1){
+        zbir=zbir +i;
+    }
+    console.log(zbir)
+    
+    
+}  
+zbirOpsega(2,7)
+
+
