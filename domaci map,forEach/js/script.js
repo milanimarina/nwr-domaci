050d9d6db5d899f668b052bf92ba74ca5c0cccb6
Milan Carapic <makzvakkvak@hotmail.com>
2022-05-15 15:46:45 +0200

    forEach i map domaci

---
+++ domaci map,forEach/js/script.js
@@ -0,0 +1,83 @@
+//zadatak 2
+
+/*let polaznici=[
+    {
+        name: 'Marko Markovic',
+        score: 90
+    },
+    {   name: 'Petar Petrovic',
+        score: 30
+
+    },
+    {
+        name: 'Stefan Stefanovic',
+        score: 50
+    }
+]
+let noviNiz=polaznici.map(polaznik=>{
+    return{
+        name: polaznik.name.toUpperCase(),
+        score: polaznik.score
+    }
+
+})
+console.log(noviNiz)*/
+
+
+//3 zadatak
+
+/*let polaznici=[
+    {
+        name: 'Marko Markovic',
+        score: 90
+    },
+    {   name: 'Petar Petrovic',
+        score: 30
+
+    },
+    {
+        name: 'Stefan Stefanovic',
+        score: 50
+    }
+]
+
+let noviNiz=polaznici.map(polaznik=>{
+    return{
+        name: polaznik.name
+        
+    }
+   
+})
+
+let polazniciString =JSON.stringify(noviNiz);
+console.log(polazniciString);*/
+
+
+
+
+
+
+ //4 zadatak
+
+ /*let niz=[1,2,3,4];
+ let noviNiz=[];
+ niz.forEach(i=>{
+    i = i *2
+    noviNiz.push(i);
+  
+ })
+ console.log(noviNiz);*/
+
+
+// 5 zadatak
+
+/*let niz=[1,2,3,4];
+let noviNiz=niz.map(i=>{
+    return String(i);
+})
+console.log(noviNiz);*/
+  
+
+
+
+
